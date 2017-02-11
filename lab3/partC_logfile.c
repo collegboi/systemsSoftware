9d72b7cfaa75cfd1d77657a8b574270fdd70fd4a
Paul Brittain <paulsabana@gmail.com>
2017-02-11 13:21:01 +0000

    Added: Lab3[ C ]

--- lab3/partC_logfile.c
+++ lab3/partC_logfile.c
@@ -2,3 +2,12 @@
 // Created by Paul Brittain on 10/02/2017.
 //
 
+#include <syslog.h>
+#include <stdio.h>
+
+void loggingFunction(void){
+    openlog("log", option, priority);
+    syslog (LOG_INFO, "invocation message");
+    closelog();
+}
+
